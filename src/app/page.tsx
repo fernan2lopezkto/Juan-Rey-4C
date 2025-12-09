35b9237b7e3a51b5b0dbfcbe056a18e8f1234dbd
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2025-12-09 19:53:10 -0300

    Actualizar page.tsx iframe directo de prueba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,8 @@ export default function Home() {
           height={"100%"}
         />
       </div>
+<div><iframe width="560" height="315" src="https://www.youtube.com/embed/eN0jYomsO_M?si=hQqCqt5SUqt22C2W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
 
     </div>
 
