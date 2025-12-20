e66a48820259e8ca09965e6c79a89f58ec68126d
Juan <fernan2lopezkto@gmail.com>
2025-12-20 00:16:45 -0300

    update hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,8 @@
-
+import HeroUno from ("@/components/HeroUno")
 export default function Home() {
   return (
     <div>
-      <div
-  className="hero min-h-screen"
-  style={{
-    backgroundImage:
-      "url('/fondoCrucesUno.jpg')",
-  }}
->
-  <div className="hero-overlay"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
+<HeroUno />
     <div className="px-12 text-center mt-12">
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
