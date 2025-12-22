9c21bece27eb756be4ba18328b56c8d64266c15a
Juan <fernan2lopezkto@gmail.com>
2025-12-22 17:51:57 -0300

    add slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import Slider from "@/components/Slider"
 import PrincipalFooter from "@/components/PrincipalFooter";
 import HeroUno from "@/components/HeroUno"
 import FilterButton from "@/components/FilterButton";
 import FooterUno from "@/components/FooterUno";
+
 export default function Home() {
   return (
     <div>
       <HeroUno />
       <FilterButton />
+      <Slider />
       <PrincipalFooter />
     </div>
 
--- src/app/utilities/page.tsx
+++ src/app/utilities/page.tsx
@@ -1,9 +1,11 @@
+import Slider from "@/component/Slider"
 import PrincipalFooter from "@/components/PrincipalFooter";
 import FooterUno from "@/components/FooterUno";
 
 export default function Utilities() {
     return (
         <div className="min-h-screen p-8">
+            <Slider />
             <PrincipalFooter />
         </div>
     );
