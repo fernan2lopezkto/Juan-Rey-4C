081e793ff49e139eceb9e260b6445ea5a4c1b82d
Juan <fernan2lopezkto@gmail.com>
2025-12-22 13:06:51 -0300

    working with footers and more

--- src/components/HeroUno.tsx
+++ src/components/HeroUno.tsx
@@ -13,15 +13,15 @@ export default function HeroUno() {
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">4C = For Christ</h1>
-          <p className="mb-5 text-2xl">
+          <p className="mb-5 ">
             Lo que antes fue Cuatro Cuerdas bajo las siglas 4C, 
             hoy se transforma en 4C - For Christ.
           </p>
-          <p className="mb-5">
+          <p className="mb-5 mt-5 italic">
             Porque de él, y por él, y para él,
             son todas las cosas.
           </p>
-          <p className="mb-5">
+          <p className="mb-5 font-bold">
             Romanos 11:36 RVR1960
           </p>
         </div>
