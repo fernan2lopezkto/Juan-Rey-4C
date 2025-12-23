47bef98cb170516ba56e064e3b45cb9e41178bab
Juan <fernan2lopezkto@gmail.com>
2025-12-23 20:17:03 -0300

    update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,10 +4,10 @@ import PrincipalFooter from "@/components/PrincipalFooter";
 export default function About() {
     return (
         <div className="min-h-screen bg-base-200 p-8">
-            <h1 classNam="text-2xl text-secondary font-semibold my-4">
+            <h1 className="text-2xl text-secondary font-semibold my-4">
                 {aboutTexts.pretitle}
             </h1>
-                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+                    <div className="grid grid-cols-1 lg:grid-cols-3 text-secondary-content gap-2">
           {aboutTexts.parrafos.map((parrafo) => (
             <p key={parrafo} className="mt-1 ">
               {parrafo}
