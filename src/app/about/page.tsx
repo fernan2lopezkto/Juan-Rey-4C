5b37e8420377b971005c14a8e774afe746615e8c
Juan <fernan2lopezkto@gmail.com>
2025-12-23 20:28:06 -0300

    preTitle

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function About() {
     return (
         <div className="min-h-screen bg-base-200 p-8">
             <h1 className="text-2xl text-secondary font-semibold my-4">
-                {aboutTexts.pretitle}
+                {aboutTexts.preTitle}
             </h1>
                     <div className="grid grid-cols-1 lg:grid-cols-3 text-secondary-content gap-2">
           {aboutTexts.parrafos.map((parrafo) => (
