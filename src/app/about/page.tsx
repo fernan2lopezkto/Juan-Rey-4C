8cc86fefcb478bc887ced77e8c45b4e0e3f3ae99
Juan <fernan2lopezkto@gmail.com>
2025-12-24 02:27:27 -0300

    update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,19 +5,21 @@ export default function About() {
     return (
         <div>
             <div
-            className="bg-opacity-50 min-h-[80vh] text-center bg-center"
+            className=" min-h-[80vh] text-center bg-center"
             style={{
             backgroundImage:"url('/fondoCrucesUno.jpg')",
             }}>
+            <div className="bg-opacity-50 bg-black">
                 {/* Titulo */}
-                <h1 className="text-3xl text-secondary font-semibold my-8">
+                <h1 className="text-3xl text-secondary font-semibold my-4">
                 {aboutTexts.preTitle}
                 </h1>
                 {/* parrafos del texto */}
-                <div className="grid grid-cols-1 lg:grid-cols-3 text-secondary-content my-4 gap-2">
+                <div className="grid grid-cols-1 lg:grid-cols-3 text-secondary-content drop-shadow my-4 gap-2">
                     {aboutTexts.parrafos.map((parrafo) => (
-                        <p key={parrafo} className="mt-1 drop-shadow">{parrafo}</p>
+                        <p key={parrafo} className="mt-1">{parrafo}</p>
                     ))}
+                </div>
                 </div>
             </div>
             <PrincipalFooter />
