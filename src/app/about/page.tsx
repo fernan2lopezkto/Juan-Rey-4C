2e24ed9e1d59b93c7a3fe8b124d406f28fc7e538
Juan <fernan2lopezkto@gmail.com>
2025-12-24 01:38:47 -0300

    text shadow y transparencia

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,23 +5,21 @@ export default function About() {
     return (
         <div>
             <div
-      className=" min-h-[80vh] bg-center"
-      style={{
-        backgroundImage:
-          "url('/fondoCrucesUno.jpg')",
-      }}
-    >
-            <h1 className="text-2xl text-secondary font-semibold my-4">
+            className="bg-opacity-50 min-h-[80vh] text-center bg-center"
+            style={{
+            backgroundImage:"url('/fondoCrucesUno.jpg')",
+            }}>
+                {/* Titulo */}
+                <h1 className="text-3xl text-secondary font-semibold my-8">
                 {aboutTexts.preTitle}
-            </h1>
-                    <div className="grid grid-cols-1 lg:grid-cols-3 text-secondary-content gap-2">
-          {aboutTexts.parrafos.map((parrafo) => (
-            <p key={parrafo} className="mt-1 drop-shadow-md">
-              {parrafo}
-            </p>
-          ))}
-        </div>
-        </div>
+                </h1>
+                {/* parrafos del texto */}
+                <div className="grid grid-cols-1 lg:grid-cols-3 text-secondary-content my-4 gap-2">
+                    {aboutTexts.parrafos.map((parrafo) => (
+                        <p key={parrafo} className="mt-1 drop-shadow">{parrafo}</p>
+                    ))}
+                </div>
+            </div>
             <PrincipalFooter />
         </div>
     );
