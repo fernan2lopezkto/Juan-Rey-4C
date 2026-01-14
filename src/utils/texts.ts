14534f8d4778084af8eba554f828c3a590b58846
Juan <fernan2lopezkto@gmail.com>
2026-01-13 21:29:29 -0300

    update texts.ts

--- src/utils/texts.ts
+++ src/utils/texts.ts
@@ -35,8 +35,8 @@ export const aboutTexts: AboutTexts = {
   preTitle: "Hola Mundo!",
   title: "For Christ",
   parrafos: [
-    "Soy Juan Rey, Uruguayo, padre de dos hermosos hijos. Fui musico, fotógrafo, emprendedor y varias cosas mas, pero hoy a través de esta plataforma, quiero aprovechar mi pasion por el desarrollo de software, para unificar ideas, proyectos y portafolio.",
-    "Con 12 añitos comencé a aprender música, mi tío Luis, me regaló un órgano y un cancionero, de esos que venían con todas las notas arriba de la letra y al final dibujos de como hacer los acordes, así comenzó algo que cambio el resto de mi vida. Luego aprendí a tocar la guitarra viendo como tocaban mis compañeros en el grupo, y hace unos cuantos años (cuando necesite tocar bajo, porque era necesario en el grupo de alabanza donde estaba) descubrí cuánto amo ese instrumento de CUATRO CUERDAS, lo que dió inicio al canal de YouTube ",
-    
+    "Soy Juan Rey, uruguayo, padre, músico,  desarrollador de software y mucho más. Mi camino musical inició a los 12 años cuando mi tío Luis me regaló un órgano y un cancionero; ese gesto cambió mi destino para siempre. Con el tiempo, aprendí guitarra observando a mis compañeros de grupo y, años después, la necesidad de tocar bajo en mi congregación me hizo descubrir un amor profundo por el instrumento de cuatro cuerdas, lo que dio vida a mi canal de YouTube.",
+    "Aunque el bajo es mi instrumento favorito, mis tutoriales se enfocaron mayormente en la guitarra, buscando ayudar a quienes recién comienzan. Mi objetivo siempre fue adaptar canciones a su forma más sencilla para motivar a la audiencia y servir como punto de partida. Hoy, utilizo esta plataforma para unificar mis ideas y portafolio, aprovechando mi formación en desarrollo de software, fotografía y logística para crear un espacio digital con sentido.",
+    'Con una nueva madurez y prioridades claras, he decidido darle un sentido más profundo al concepto de "4C". Lo que antes significaba simplemente "Cuatro Cuerdas", hoy se transforma en "For-Christ" (Para Cristo). Mi identidad no reside en mis oficios, sino en Jesucristo, reconociendo que de Él, por Él y para Él son todas las cosas. Este sitio es el reflejo de esa transición: una mezcla de tecnología, arte y propósito bajo Su guía.',
   ],
 };
