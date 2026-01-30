9ae8f620a9caf2fb32cc524e8187080987218880
Juan <fernan2lopezkto@gmail.com>
2026-01-30 19:46:26 -0300

    update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,1 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  // Aquí defines qué rutas quieres proteger. 
-  // Si un usuario no está logueado e intenta entrar aquí, será enviado al /login.
-  matcher: [
-    "/profile/:path*", 
-    // Puedes agregar más rutas aquí, por ejemplo:
-    // "/dashboard/:path*",
-  ],
-};
+export { auth as middleware } from "@/auth"
